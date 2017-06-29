1954287f3679fd7a986127c06b11ace176f10353
shivPadhi <shivashispadhi@gmail.com>
2017-06-29 13:17:08 +0530

    Edit url for connecting to mongoDB

--- config/mongoInit.js
+++ config/mongoInit.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose') ;
 
 module.exports.initDb = function(){
-	mongoose.connect('mongodb://localhost:27017/travelDiary') ;
+	mongoose.connect('mongodb://localhost:27017/traveldiary') ;
 	var db = mongoose.connection ;
 
     db.on('error', console.error.bind(console, 'connection error:'));
