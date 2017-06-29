167d2244d08ffe09657ce047a0334494c9c658a4
shivPadhi <shivashispadhi@gmail.com>
2017-06-29 21:03:53 +0530

    Add handler for ESC button

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,8 +2,8 @@ var $create = $('.create')
 var $browse = $('.browse')
 var $createBoard = $('.create-board')
 var $browseBoard = $('.browse-board')
-var map1 = document.getElementById('map1')
-
+var map1 = document.getElementById('map1') ;
+var addForm = document.querySelector('.addForm') ;
 $create.click(function(){
 	$createBoard.css('display','block')
 	$browseBoard.css('display','none')
@@ -15,3 +15,12 @@ $browse.click(function(){
 	$browseBoard.css('display','block')
 })
 
+function showForm(){
+	addForm.style.display = 'block' ;
+}
+
+window.onkeydown = function(e){
+	if(e.keyCode === 27){
+		addForm.style.display = 'none' ;
+	}
+}
