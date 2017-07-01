12e79483fdb1c746b0af59125aa99bf3ae8da7a3
shivPadhi <shivashispadhi@gmail.com>
2017-07-01 09:26:46 +0530

    Add new route to handle single detatils journal

--- routes/serve_json.js
+++ routes/serve_json.js
@@ -15,7 +15,17 @@ router.get('/public_journals', function(req, res, next){
 router.post('/journal_details', function(req, res, next){
 	console.log('Data requested')
 	var position = req.body.lat_lng;
-	Journal.findOne({position:position}, function(err, journal){
+	Journal.find({position:position}, function(err, journals){
+		res.json({journals:journals})
+	})
+})
+router.post('/journal_single_details', function(req, res, next){
+	console.log('Data requested')
+	Journal.findOne({_id:req.body._id}, function(err, journal){
+		if(err){
+			console.log(err)
+		}
+		console.log(journal, 'awesome')
 		res.json({journal:journal})
 	})
 })
